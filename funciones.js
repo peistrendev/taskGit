f6b73ec748b2af11148d205db95abd7f8628a38d
peistrendev <1001.30105189.ucla@gmail.com>
2025-04-09 06:51:33 -0400

    Archivo JS funciones

---
+++ funciones.js
@@ -0,0 +1,11 @@
+
+const filtrarMayoresDeDiez = numeros => numeros.filter(num => num > 10);
+
+// Ejemplo de uso:
+const lista = [5, 12, 8, 20, 3];
+const mayoresDeDiez = filtrarMayoresDeDiez(lista);
+
+console.log(mayoresDeDiez); // [12, 20]
+
+
+
